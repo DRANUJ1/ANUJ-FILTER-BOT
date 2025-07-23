0b0ee0e5c8b821cb169f243361bba167573e32c8
DRANUJ1 <anujediter910@gmail.com>
2025-07-23 23:24:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
-if [ -z $UPSTREAM_REPO ]
-then
+#!/bin/bash
+
+# Clone repo only if UPSTREAM_REPO is set
+if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
-  https://github.com/DRANUJ1/ANUJ-FILTER-BOT
+  git clone https://github.com/DRANUJ1/ANUJ-FILTER-BOT /Anuj-filter-bot
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Anuj-filter-bot
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" /Anuj-filter-bot
 fi
-cd /Edith-JFBdec
+
+# Go to repo directory
+cd /Anuj-filter-bot || exit
+
+# Install dependencies
 pip3 install -U -r requirements.txt
+
+# Start bot
 echo "Starting Anuj filter bot...."
 python3 bot.py
