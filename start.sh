4a67d65f7102cadb1908f3cd4cab5ad119408d92
DRANUJ1 <anujediter910@gmail.com>
2025-07-24 02:27:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
-cd /
+#!/bin/bash
+# cd /  <-- Is line ko hata dein ya comment kar dein
+
+echo "Updating dependencies..."
 pip3 install -U -r requirements.txt
+
 echo "Starting Anuj filter bot...."
 python3 bot.py
