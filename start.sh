4a9c500908e8218229b56b89d7d7aaf8f925bd63
DRANUJ1 <anujediter910@gmail.com>
2025-07-23 23:32:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Clone repo only if UPSTREAM_REPO is set
-if [ -z "$UPSTREAM_REPO" ]; then
-  echo "Cloning main Repository"
-  git clone https://github.com/DRANUJ1/ANUJ-FILTER-BOT /Anuj-filter-bot
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO"
-  git clone "$UPSTREAM_REPO" /Anuj-filter-bot
-fi
-
 # Go to repo directory
 cd /Anuj-filter-bot || exit
 
