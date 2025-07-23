2b380c85419ebfe20812361db8053c8e718c3e1e
DRANUJ1 <anujediter910@gmail.com>
2025-07-23 23:37:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-
-# Go to repo directory
-cd /Anuj-filter-bot || exit
-
-# Install dependencies
+cd /
 pip3 install -U -r requirements.txt
-
-# Start bot
-echo "Starting Anuj filter bot...."
+echo "Starting Jisshu filter bot...."
 python3 bot.py
